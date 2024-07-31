b81a01a81bb18e310ad924dd6f6bee013f2e3fca
Victor Vicentini Cunha <159261514+VictorCunha26@users.noreply.github.com>
2024-07-31 09:21:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = '1.png'
+    legendas.innerHTML= 'Entrada da cidade'
+    
+}
+function imagem2(){
+
+    lamp. src = '2.png'
+    legendas.innerHTML= 'Praia mais conhecida'
+    
+}
+
+function imagem3(){
+
+    lamp.src = '3.png'
+    legendas.innerHTML= 'Escadão de Arrail do Cabo'
+}
+function imagem4(){
+
+    lamp. src = '4.png'
+    legendas.innerHTML= 'Centro da cidade'
+}
